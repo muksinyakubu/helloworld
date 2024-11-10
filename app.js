2a46ad6f44f6b39fe4cb79ce7603e593a695a663
muksinyakubu <muksinyakubu@gmail.com>
2024-11-10 09:08:10 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const port = 5050
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+
+
+app.listen(port,()=>{
+    console.log("Started the server");
+})
